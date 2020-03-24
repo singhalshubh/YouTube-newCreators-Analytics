0c4655f9f93150f41a661c9b22bbcff4959f4df8
singhalshubh <shubhendrapalsinghal@gmail.com>
2020-03-25 05:18:00 +0530

    COOKIE testing
    COOKIE passed from javascript to php(in another dile and same file)
    
    path=/ is for directories...not for files.
    Specify directory as /test..and sample.php will work..same page will work after refresh.

---
+++ testCookiephp/index.php
@@ -0,0 +1,16 @@
+<?php
+	echo $_COOKIE["username"];
+?>
+
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8">
+<head>
+	<title></title>
+</head>
+<body>
+<script type="text/javascript">
+	document.cookie = "username=shubhendra;path=/test";
+</script>
+</body>
+</html>
